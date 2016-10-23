2ff491f645c3b1fd6ee6f4443735f3f6858f6815
Yongzheng Huang <hahooy1@Yongzhengs-MacBook-Air.local>
2016-10-22 19:13:56 -0500

    implemented search shelter

--- server_side/homesort/homesort/urls.py
+++ server_side/homesort/homesort/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+import django.views.defaults
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('homesort_app.urls')),
+    url(r'^404/$', django.views.defaults.page_not_found)
 ]
